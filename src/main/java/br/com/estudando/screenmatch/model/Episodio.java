289e3befc0d064c73ff385e9a1e4af1a963a3833
ericdenser <ericdenser7@gmail.com>
2025-07-25 17:17:50 -0300

    Implementando busca de episodio específico por trechos do título

---
+++ src/main/java/br/com/estudando/screenmatch/model/Episodio.java
@@ -0,0 +1,81 @@
+package br.com.estudando.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private Double avaliacao;
+    private LocalDate dataDeLancamento;
+
+    public Episodio(Integer temporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = temporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.episodio();
+
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException e) {
+            this.avaliacao = 0.0;
+        }
+
+        try {
+            this.dataDeLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeParseException e) {
+            this.dataDeLancamento = null;
+        }
+
+    }
+
+    public LocalDate getDataDeLancamento() {
+        return dataDeLancamento;
+    }
+
+    public void setDataDeLancamento(LocalDate dataDeLancamento) {
+        this.dataDeLancamento = dataDeLancamento;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao=" + avaliacao +
+                ", dataDeLancamento=" + dataDeLancamento +
+                '}';
+    }
+}
--- src/main/java/br/com/estudando/screenmatch/principal/Principal.java
+++ src/main/java/br/com/estudando/screenmatch/principal/Principal.java
@@ -3,13 +3,11 @@ package br.com.estudando.screenmatch.principal;
 import br.com.estudando.screenmatch.model.DadosEpisodio;
 import br.com.estudando.screenmatch.model.DadosSerie;
 import br.com.estudando.screenmatch.model.DadosTemporada;
+import br.com.estudando.screenmatch.model.Episodio;
 import br.com.estudando.screenmatch.services.ConsumoAPI;
 import br.com.estudando.screenmatch.services.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -52,5 +50,25 @@ public class Principal {
                 .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
                 .limit(5)
                 .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("Digite um trecho do titulo do episodio para busca-lo: ");
+        var trechoTitulo = read.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                .findFirst();
+        if (episodioBuscado.isPresent()) {
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada()
+            + "\nEpisódio: " + episodioBuscado.get().getTitulo() );
+        } else {
+            System.out.println("Episodio nao encontrado");
+        }
     }
 }
